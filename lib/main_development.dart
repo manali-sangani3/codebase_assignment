fef8de266c9fb479b10b183cad812fae76f9c8a7
Manali Sangani <manali.sangani@neosoftmail.com>
2025-02-27 10:33:37 +0530

    add hive initialisation in all flavors

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/adapters.dart';
 
+import 'app/model/user_data_entity.dart';
 import 'application.dart';
 import 'config/flavor_config.dart';
 import 'config/resources/env_assets.dart';
@@ -15,6 +18,9 @@ void main() async {
 
   // Ensure all services are loaded before app is started.
   WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  Hive.registerAdapter(UserDataAdapter()); // Register adapter
+  await Hive.openBox<List>('usersBox'); // Open Hive Box
 
   runApp(const Application());
 }
--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_base_project/config/resources/env_assets.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/adapters.dart';
 
+import 'app/model/user_data_entity.dart';
 import 'application.dart';
 import 'config/flavor_config.dart';
 
@@ -17,6 +20,9 @@ void main() async {
 
   // Ensure all services are loaded before app is started.
   WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  Hive.registerAdapter(UserDataAdapter()); // Register adapter
+  await Hive.openBox<List>('usersBox'); // Open Hive Box
 
   runApp(const Application());
 }
